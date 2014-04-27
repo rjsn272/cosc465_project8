3dea061fe5c72280980aff9ad433afebfa638039
Ross Nicholson <rnicholson@colgate.edu>
2014-04-27 13:01:22 -0400

    Working on stage two

---
+++ userdb.js
@@ -0,0 +1,27 @@
+var userhash = { };
+var next_anonymous = 1; 
+
+var add_user = function(id, user) {
+    if (userhash[id] === undefined) {
+        if (!user) {
+            user = "anonymous" + next_anonymous;
+            next_anonymous += 1;
+        }
+        userhash[id] = {
+            'id': id,
+            'user': user,
+            'latency_results': []
+        };
+    }
+    return userhash[id];
+};
+exports.add_user = add_user;
+
+exports.get_user_name = function(id,rtt) {
+    if (userhash[id] === undefined) {
+        add_user(id, undefined);
+	userhash[id].latency_results = [rtt];
+    }
+    return userhash[id].user;
+};
+
