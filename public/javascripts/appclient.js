b2a9ea72cb8b9976901f6cc3d61183c25c4b0f47
Ross Nicholson <rnicholson@colgate.edu>
2014-04-28 19:31:04 -0400

    Ping/Pong updates now installed

--- public/javascripts/appclient.js
+++ public/javascripts/appclient.js
@@ -4,10 +4,21 @@ var myapp = (function(){
 	var initPing = function() {
 		socket = io.connect();
 		var aveRTT = 0;
+		var displayLine;
+		var testNo;
 		socket.on('pong', function(data) {
         	var rtt = Date.now() - data.timestamp;
         	console.log("Ping RTT (milliseconds): " + rtt);
 			console.log("Dat seq: " + data.seq);
+			if (data.seq == 5) testNo = '1';
+			else if (data.seq == 4) testNo = '2';
+			else if (data.seq == 3) testNo = '3';
+			else if (data.seq == 2) testNo = '4';
+			else if (data.seq == 1) testNo = '5';
+
+			displayLine = "Ping/Pong No. " + testNo + " Time: " + rtt + " milliseconds"
+			testNo = '#test'+testNo; 
+			jQuery(testNo).html(displayLine);
 			data.seq--;
 
 			if (data.seq >= 1) {
@@ -22,7 +33,8 @@ var myapp = (function(){
 				aveRTT = aveRTT/5;
 				console.log("Final aveRTT: ",aveRTT);
 				socket.emit('result', {finalRTT: aveRTT});
-				jQuery('#stoptest').html(aveRTT);
+				displayLine = "Average RTT: " + aveRTT + " milliseconds"
+				jQuery('#finaltest').html(displayLine);
 				aveRTT = 0;
 			}
 
