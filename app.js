05453e7ec45bd5c92a0680f3e80abec48939c12e
Ross Nicholson <rnicholson@colgate.edu>
2014-04-28 20:41:38 -0400

    Added beginning of google authentication

--- app.js
+++ app.js
@@ -36,5 +36,28 @@ var views = require('./views.js');
 app.get('/', views.index);
 app.get('/tests', views.tests);
 
+var passport = require('./node_modules/passport')
+  , GoogleStrategy = require('./node_modules/passport-google').Strategy;
+
+passport.use(new GoogleStrategy({
+    returnURL: 'http://localhost:3000/auth/google/return',
+    realm: 'http://localhost:3000/'
+  },
+  function(identifier, profile, done) {
+    User.findOrCreate({ openId: identifier }, function(err, user) {
+      done(err, user);
+    });
+  }
+));
+// Redirect the user to Google for authentication.  When complete, Google
+// will redirect the user back to the application at
+//     /auth/google/return
+app.get('/auth/google', passport.authenticate('google'));
+
+// Google will redirect the user to this URL after authentication.  Finish
+// the process by verifying the assertion.  If valid, the user will be
+// logged in.  Otherwise, authentication has failed.
+app.get('/auth/google/return', passport.authenticate('google', { successRedirect: '/', failureRedirect: '/login' }));
+
 app.listen(port);
 console.log("App started on port " + port);
